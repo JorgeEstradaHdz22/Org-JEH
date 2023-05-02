c06906a8e6a201f78211230893e0ff2396d62128
Jorge Estrada <pachecoestradalopz21@gmail.com>
2023-05-01 19:21:30 -0600

    Incio del Curso: React: Como los Componentes Funcionan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import Header from  './components/Header';
+import Formulario from './components/Formulario';
+import MiOrg from './components/MiOrg';
 
 function App() {
+
+  const [mostrarFormulario,actualizarMostrar] = useState(true);
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {mostrarFormulario && <Formulario />}
+      <MiOrg cambiarMostrar={cambiarMostrar} />
     </div>
   );
 }
---
+++ src/components/ListaOpciones/index.js
@@ -0,0 +1,29 @@
+import './ListaOpciones.css';
+
+const ListaOpciones = (props) => {
+    
+    const equipos = [
+        'Programación',
+        'Front End',
+        'Data Science',
+        'Devops',
+        'UX y Diseño',
+        'Móvil',
+        'Inovación y Gestión',
+    ];
+
+    const manejarCambio = (e) => {
+        console.log('Cambio',e.target.value);
+        props.actualizarEquipo(e.target.value);
+    };
+
+    return <div className='lista-opciones'>
+        <label>Equipos</label>
+        <select value={props.valor} onChange={manejarCambio}>
+            <option value='' disabled defaultValue='' hidden>Seleccionar Equipo</option>
+            {equipos.map((equipo,index) => <option key={index} value={equipo}>{equipo}</option>)}
+        </select>
+    </div>
+};
+
+export default ListaOpciones;
